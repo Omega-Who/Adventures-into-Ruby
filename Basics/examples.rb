b10b8a282ec9cecb6d57751918e56b8977eccaac
Omega-Who <22parkerc@gmail.com>
2022-03-14 21:34:07 -0800

    Renamed main.rb to examples.rb

---
+++ Basics/examples.rb
@@ -0,0 +1,27 @@
+# Prints to the Console
+puts "It's Alive!"
+
+#Math Output
+puts "You have #{25+30/6} Rupees in your pocket."
+
+# Variables and Names
+cars = 100
+drivers = 30
+puts " There are #{cars} cars and #{drivers} drivers."
+
+# Getting Inputs
+print " How old are you? "
+age = gets.chomp
+print " How tall are you? "
+height = gets.chomp
+puts " You are #{age} years old and your height is #{height} cms?"
+
+# Prompting Numbers
+print "Give a number,"
+number = gets.chomp.to_i
+puts "You just entered #{number}."
+
+#Multi Line Comment
+=begin
+inbetween
+=end
