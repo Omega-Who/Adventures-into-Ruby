7e9def74efcad455f829221b412a5ca35ddd829a
omega <58758022+Omega-Who@users.noreply.github.com>
2022-03-17 22:11:00 +0000

    Compress

--- Scripts/printer.rb
+++ Scripts/printer.rb
@@ -1,8 +1,7 @@
 # Printer Script
 for x in 1..Float::INFINITY
-	if x+1 > Float::INFINITY then	
+	if x+1 > Float::INFINITY then
 		next
-	end	
-	# Print
-	puts "Line #{x*1}, Printed."
+	end
+	puts "#{x}"
 end
